74f25faa5a2e7611e32838a0964fd1a22c5a1cd0
Devendra Selokar <selokardevendra05@gmail.com>
2024-09-11 18:55:13 +0530

    commiting code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,24 +25,3 @@ const Expenses = (props) => {
 
 export default Expenses
 
-
- {/* <ExpenseItem 
-        date={item[0].date}
-        title={item[0].title}
-        amount={item[0].amount}
-      />
-      <ExpenseItem 
-        date={item[1].date}
-        title={item[1].title}
-        amount={item[1].amount}
-      />
-      <ExpenseItem 
-        date={item[2].date}
-        title={item[2].title}
-        amount={item[2].amount}
-      />
-      <ExpenseItem 
-        date={item[3].date}
-        title={item[3].title}
-        amount={item[3].amount}
-      /> */}
